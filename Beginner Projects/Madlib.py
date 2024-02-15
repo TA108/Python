7bb13f83c87fccd621876dec45c03ec429139bce
Tanmay Arya <aryatanmay108@gmail.com>
2024-02-15 13:22:14 +0530

    Add files via upload

---
+++ Beginner Projects/Madlib.py
@@ -0,0 +1,23 @@
+Name = input('Enter Your name: ')
+Verb_1 = input('Enter a Verb: ')
+Adj_1 = input('Enter a Adjective: ')
+Verb_2 = input('Enter a Verb: ')
+Adj_2 = input('Enter a Adjective: ')
+Verb_3 = input('Enter a Verb: ')
+Adj_3 = input('Enter a Adjective: ')
+Verb_4 = input('Enter a Verb: ')
+Adj_4 = input('Enter a Adjective: ')
+Verb_5 = input('Enter a Verb: ')
+Adj_5 = input('Enter a Adjective: ')
+Verb_6 = input('Enter a Verb: ')
+Adj_6 = input('Enter a Adjective: ')
+
+Madlib = f"""A man {Verb_1} in salt. He had an {Adj_1} to {Verb_2} loads of salt from a {Adj_2} town. 
+There was a stream on the way. The {Adj_3} had to {Verb_3} that river {Adj_4} day. 
+One day the {Adj_5} {Verb_4} into the river.
+Some of the salt {Verb_5} in the water. The load {Verb_6} {Adj_6}."""
+
+print(Madlib)
+
+# source https://englishluv.com/paragraph-stories/
+# The man and his ass
