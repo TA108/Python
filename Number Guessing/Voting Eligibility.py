1ee4fc22ba230028ea994ea2021223f939c7e4e9
Tanmay Arya <aryatanmay108@gmail.com>
2024-02-15 12:44:12 +0530

    Delete Number Guessing directory

--- Number Guessing/Voting Eligibility.py
+++
@@ -1,7 +0,0 @@
-name = input('Enter name:')
-age = int(input('Enter age:'))
-
-if age >= 18:
-    print(name, "you can vote as you are of age", age)
-else:
-    print(name, "you can't vote as you are of age", age)
